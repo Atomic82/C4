8d983370d1c24d616ad200e53f88c25e1ea387eb
Atomic82 <tom@atomic82.com>
2022-02-14 21:30:15 -0500

    Push code

--- js/app.js
+++ js/app.js
@@ -78,6 +78,7 @@ const winningCombos = [
 const playerRed = 1
 const playerYellow = -1
 let winner, turn, circleArray
+let numOfTurns
 // let numOfTurns
 
 
@@ -96,7 +97,7 @@ gameBoard.forEach(function(circle){
 
 
 /*-------------------------------- Functions --------------------------------*/
-init()
+
 
 function handleClick(event) {
   let circleIndex = parseInt(event.target.id);
@@ -105,6 +106,9 @@ function handleClick(event) {
   circleArray[correctIdx] = turn;
 
   turn = turn * -1;
+
+  const turnMsg = turn === 1 ? 'red' : 'white'
+    message.textContent = `Turn: ${turnMsg}`
 
   // if (winner) {
   //   return;
@@ -181,6 +185,9 @@ function getWinner(){
             console.log('White wins')
           message.textContent = 'White wins';
             winner = 'White'
+          } else (winner === null) {
+            console.log('Tie')
+            message.textContent = 'Tie';
           }
   }  
   
@@ -191,6 +198,7 @@ function getWinner(){
   // }
   
 }
+init()
 
 // resetBtn.addEventListener('click', init)
 
